f9693836022a7af5551aa9d3741d248d18655b39
monsalvo96 <horaciomonsalvo@gmail.com>
2019-05-28 21:10:00 -0300

    librerias bien llamada

--- application/views/index.php
+++ application/views/index.php
@@ -12,7 +12,7 @@
   <title>Rescatista</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="vendor/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
@@ -651,7 +651,7 @@
   <script src="<?php echo base_url();?>assets/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="<?php echo base_url();?>assets/js/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.easing.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="<?php echo base_url();?>assets/js/template/sb-admin-2.min.js"></script>
